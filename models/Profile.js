394ad15333fd374a6a7bbe1684c089171a4427a2
tatsusei <doubledragontaz@gmail.com>
2020-04-29 23:09:39 +0900

    Format file

--- models/Profile.js
+++ models/Profile.js
@@ -4,107 +4,42 @@ const ProfileSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
   },
-  company: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-  skills: {
-    type: [String],
-  },
-  bio: {
-    type: String,
-  },
-  githubusername: {
-    type: String,
-  },
+  company: { type: String },
+  location: { type: String },
+  status: { type: String, required: true },
+  skills: { type: [String] },
+  bio: { type: String },
+  githubusername: { type: String },
   experience: [
     {
-      title: {
-        type: String,
-        required: true,
-      },
-      company: {
-        type: String,
-        require: true,
-      },
-      location: {
-        type: String,
-      },
-      from: {
-        type: Date,
-        required: true,
-      },
-      to: {
-        type: Date,
-        required: true,
-      },
-      current: {
-        type: Boolean,
-        default: false,
-      },
-      description: {
-        type: String,
-      },
+      title: { type: String, required: true },
+      company: { type: String, require: true },
+      location: { type: String },
+      from: { type: Date, required: true },
+      to: { type: Date, required: true },
+      current: { type: Boolean, default: false },
+      description: { type: String },
     },
   ],
   education: [
     {
-      school: {
-        type: String,
-        required: true,
-      },
-      degree: {
-        type: String,
-        require: true,
-      },
-      fieldofstudy: {
-        type: String,
-        require: true,
-      },
-      from: {
-        type: Date,
-        required: true,
-      },
-      to: {
-        type: Date,
-        required: true,
-      },
-      current: {
-        type: Boolean,
-        default: false,
-      },
-      description: {
-        type: String,
-      },
+      school: { type: String, required: true },
+      degree: { type: String, require: true },
+      fieldofstudy: { type: String, require: true },
+      from: { type: Date, required: true },
+      to: { type: Date, required: true },
+      current: { type: Boolean, default: false },
+      description: { type: String },
     },
   ],
   social: {
-    youtube: {
-      type: String,
-    },
-    twitter: {
-      type: String,
-    },
-    facebook: {
-      type: String,
-    },
-    linkedin: {
-      type: String,
-    },
-    instagram: {
-      type: String,
-    },
+    youtube: { type: String },
+    twitter: { type: String },
+    facebook: { type: String },
+    linkedin: { type: String },
+    instagram: { type: String },
   },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+  date: { type: Date, default: Date.now },
 });
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
