8a6744e12b8f5c4fe6f0a602440988d9b9753cd9
tatsusei <doubledragontaz@gmail.com>
2020-05-01 10:34:35 +0900

    Alert Reducer Action types

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,58 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('SUCCESS');
+  };
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Sign Into Your Account
+      </p>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p>
+        Don't have an account? <Link to='/register'>Sign up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
