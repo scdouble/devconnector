e6b6b7bca2b18e2ecfc385a347bcc2fb4fb597e4
tatsusei <doubledragontaz@gmail.com>
2020-05-04 21:58:56 +0900

    Comment Display Delete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
-import PostForm from '../posts/PostForm';
 import CommentFrom from './CommentForm';
 import { Link } from 'react-router-dom';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -21,6 +21,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentFrom postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
