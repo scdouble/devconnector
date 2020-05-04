21aa5850f4d3e4ee641106a228a5ca3e873813a1
tatsusei <doubledragontaz@gmail.com>
2020-05-04 23:02:54 +0900

    prepared for deployment

--- server.js
+++ server.js
@@ -2,21 +2,29 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db');
-
+const path = require('path');
 // Connect Databae
 connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-app.get('/', (req, res) => {
-  res.send('API Running!');
-});
+// app.get('/', (req, res) => {
+//   res.send('API Running!');
+// });
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => [
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
+  ]);
+}
 app.listen(PORT, () => {
-  console.log(`🚀 Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
